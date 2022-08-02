fa8672428d245815158ba58ac6689ebbfb69226e
Suraj450 <49590906+Suraj450@users.noreply.github.com>
2022-08-02 16:39:17 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] s){
+        System.out.println("hello All");
+        for(int i=0;i<5; i++){
+          System.out.println("value of i : " + i); 
+        }
+    }
+}
